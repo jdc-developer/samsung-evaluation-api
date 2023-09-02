5207467866adfa5b4f7b8af47f4fe21b1a002f2b
Jorge do Carmo <jorge.do.carmo123@gmail.com>
2023-09-02 18:00:47 -0300

    Project created

---
+++ src/test/java/com/ibm/SamsungEvaluationAppTests.java
@@ -0,0 +1,7 @@
+package com.ibm;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SamsungEvaluationApp.class)
+public class SamsungEvaluationAppTests {
+}
